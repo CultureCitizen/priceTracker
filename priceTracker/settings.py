c8e0b744b356113a942332fae5dcfb37436faad1
Armando de la Torre <adelatorremothelet@gmail.com>
2022-04-23 01:06:42 -0500

    static files problem

--- priceTracker/settings.py
+++ priceTracker/settings.py
@@ -160,8 +160,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-static_root = os.path.join(BASE_DIR, 'staticfiles')
-print(static_root)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print("static root:", STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
